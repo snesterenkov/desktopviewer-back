1f8aa05d0f35b63247270d5a0cdc1b96f858dba9
coach88 <alexsemenyuk88@gmail.com>
2015-08-18 13:04:42 +0300

    Fixed time converter.

--- business-layout/src/main/java/com/eklib/desktopviewer/serializer/CustomTimeSerializer.java
+++ business-layout/src/main/java/com/eklib/desktopviewer/serializer/CustomTimeSerializer.java
@@ -16,7 +16,7 @@ public class CustomTimeSerializer extends JsonSerializer<Date> {
     @Override
     public void serialize(Date value, JsonGenerator gen, SerializerProvider arg2) throws
             IOException {
-        SimpleDateFormat formatter = new SimpleDateFormat("hh:mm");
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
         String formattedTime = formatter.format(value);
         gen.writeString(formattedTime);
     }
