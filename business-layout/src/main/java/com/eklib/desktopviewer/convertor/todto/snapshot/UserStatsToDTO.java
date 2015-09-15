ccabf6d28e6dba87f0e4925222f7ecd50fe93922
V.Pisetskiy <rockman52@ya.ru>
2015-09-15 17:01:25 +0300

    Created converter to UserStatsDTO from Object[]

---
+++ business-layout/src/main/java/com/eklib/desktopviewer/convertor/todto/snapshot/UserStatsToDTO.java
@@ -0,0 +1,27 @@
+package com.eklib.desktopviewer.convertor.todto.snapshot;
+
+import com.eklib.desktopviewer.dto.snapshot.UserStatsDTO;
+import com.google.common.base.Function;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * Created by s.sheman on 15.09.2015.
+ */
+@Component
+public class UserStatsToDTO implements Function<Object[], UserStatsDTO> {
+
+    @Override
+    public UserStatsDTO apply(Object[] objects) {
+        if(objects == null){
+            return null;
+        }
+        UserStatsDTO userStatsDTO = new UserStatsDTO();
+        userStatsDTO.setUserId((Long) objects[0]);
+        userStatsDTO.setStartTime((Date) objects[1]);
+        userStatsDTO.setSnapshotsCount((Long) objects[3] );
+
+        return userStatsDTO;
+    }
+}
