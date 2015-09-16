a023864e537239408f9377e9ae462368fa5ecf0a
V.Pisetskiy <rockman52@ya.ru>
2015-09-16 16:52:32 +0300

    Created controller for getting current time

---
+++ rest-api/src/main/java/com/eklib/desktopviewer/api/v1/time/TimeController.java
@@ -0,0 +1,25 @@
+package com.eklib.desktopviewer.api.v1.time;
+
+import com.eklib.desktopviewer.services.time.TimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by s.sheman on 16.09.2015.
+ */
+@RestController
+@RequestMapping("/time")
+public class TimeController {
+
+    @Autowired
+    private TimeService timeService;
+
+    @RequestMapping(method = RequestMethod.GET, headers="Accept=application/json")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Long getCurrentTime() {
+        return timeService.getCurrentTime();
+    }
+
+}
